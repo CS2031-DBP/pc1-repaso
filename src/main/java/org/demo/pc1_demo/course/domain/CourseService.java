769a9c772a5d9d18fe593fd90e386828fdbf7a09
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-05-15 22:52:36 -0500

    Repasito

---
+++ src/main/java/org/demo/pc1_demo/course/domain/CourseService.java
@@ -0,0 +1,7 @@
+package org.demo.pc1_demo.course.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+}
---
+++ src/main/java/org/demo/pc1_demo/professor/application/ProfessorController.java
@@ -0,0 +1,10 @@
+package org.demo.pc1_demo.professor.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfessorController {
+
+
+}
