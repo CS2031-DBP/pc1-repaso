34cffc09427413139147c8cfa078000b102ba0bb
mateonoel2 <mateo.noel@utec.edu.pe>
2024-05-25 13:34:40 -0500

    cors

--- src/test/java/org/demo/pc1_demo/Pc1DemoApplicationTests.java
+++ src/test/java/org/demo/pc1_demo/Pc1DemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Pc1DemoApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
